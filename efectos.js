be30b7f63c87011914aeb6d4fa8b6e9a1fce2c02
LucianoBustos16 <45924908+LucianoBustos16@users.noreply.github.com>
2021-03-08 12:53:13 -0300

    Update efectos.js

--- efectos.js
+++ efectos.js
@@ -4,6 +4,6 @@ $(window).scroll(function(){
 			'transform': 'translate(0px,' + scroll / 3 + '%)'
 		});
         $('.gradient').css({
-			'opacity': (.7 - scroll/100)
+			'opacity': (.7 - scroll/1000)
 		});
 });
